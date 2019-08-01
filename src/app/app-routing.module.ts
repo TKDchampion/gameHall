e2b4269b871d36754e4bbc723e92604a5eb6b561
TKDchampion <as1219123@yahoo.com.tw>
2019-08-01 11:31:58 +0800

    fix pages routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,7 @@ import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
   { path: '', redirectTo: 'pages', pathMatch: 'full' },
-  { path: 'pages', loadChildren: () => import('./pages/pages.module').then(m => m.PagesModule) }
+  { path: 'pages', loadChildren: './pages/pages.module#PagesModule' }
 ];
 
 @NgModule({
